74bd1887bc8b25e22389cdf7e53b1bff558be21a
stefanstanimirovic <stanimirovic.stefan@gmail.com>
2021-02-09 18:05:05 +0100

    PHP + MYSQL

---
+++ 21_PHP+MYSQL/index.php
@@ -0,0 +1,14 @@
+<?php
+    // Kreiranje objekta konekcije
+
+    $servername = "localhost";
+    $username = "admin";
+    $password = "admin123";
+    $db = "bolnica";
+
+    $conn = new mysqli($servername, $username, $password, $db);
+    if($conn->connect_error) {
+        die("Error connecting to database: " . $conn->connect_error);
+    }
+
+    echo "Successfully connected!";
