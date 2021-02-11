a85c2b87e3006f09eddb9bc2d6e29867a5fb1a43
stefanstanimirovic <stanimirovic.stefan@gmail.com>
2021-02-11 18:06:21 +0100

    Promena foldera

--- 21_PHP+MYSQL/index.php
+++ 21_PHP+MYSQL/index.php
@@ -1,14 +1,41 @@
-<?php
-    // Kreiranje objekta konekcije
-
-    $servername = "localhost";
-    $username = "admin";
-    $password = "admin123";
-    $db = "bolnica";
-
-    $conn = new mysqli($servername, $username, $password, $db);
-    if($conn->connect_error) {
-        die("Error connecting to database: " . $conn->connect_error);
-    }
-
-    echo "Successfully connected!";
+<?php 
+    require_once "connection.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+        // Prikazati sve pacijente u tabeli
+        $q = "SELECT * FROM pacijenti";
+        $result = $conn->query($q);
+        if(!$result->num_rows) {
+            echo "<p class='info'>Trenutno u bazi nemate pacijenata!</p>";
+        }
+        else {
+            echo "<table class='tabela'>";
+            echo "<tr>
+                <th>Ime</th>
+                <th>Prezime</th>
+                <th>Visina</th>
+                <th>Težina</th>
+            </tr>";
+            foreach ($result as $row) {
+                echo "<tr>";
+                echo "<td>" . $row['ime'] . "</td>";
+                echo "<td>" . $row['prezime'] . "</td>";
+                echo "<td>" . $row['visina'] . "</td>";
+                echo "<td>" . $row['tezina'] . "</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+        }
+    ?>
+</body>
+</html>
